c2026543f3f292e2f38960adb459fa23a1e4e2f1
ahamd-akbar <ahmadi.akbar.1996@gmail.com>
2019-06-11 21:19:12 +0430

    added PersianCalendarExample Folder

---
+++ PersianCalendarExample/theme.js
@@ -0,0 +1,28 @@
+const theme = {
+  dark: {
+    calendarBackground: "#333248",
+    textSectionTitleColor: "#ff7296",
+    dayTextColor: "#ffde03",
+    todayTextColor: "#00c853",
+    selectedDayTextColor: "white",
+    monthTextColor: "white",
+    selectedDayBackgroundColor: "#333248",
+    arrowColor: "#9c27b0",
+    textDayFontSize: 20,
+    textMonthFontSize: 24,
+    textDayHeaderFontSize: 16
+  },
+  light: {
+    textSectionTitleColor: "#f06292",
+    dayTextColor: "#6600ee",
+    todayTextColor: "#00f893",
+    selectedDayTextColor: "white",
+    selectedDayBackgroundColor: "#4caf50",
+
+    textDayFontSize: 20,
+    textMonthFontSize: 24,
+    textDayHeaderFontSize: 16
+  }
+};
+
+export default theme;
